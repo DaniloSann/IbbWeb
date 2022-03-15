0b88dafdbb524ec9ff260f420f135a77819f5702
Danilo Santana <danilosan262020@gmail.com>
2022-03-15 00:45:56 -0300

    Projeto criado

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+$( document ).ready(function() {
+    setTimeout(function() {
+        $('#img1').parallax({imageSrc: '../img/oracao.jpg'});
+        $('#apply-area').parallax({imageSrc: 'img/pattern.png'});
+      }, 200);
+});
